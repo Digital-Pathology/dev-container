25c71be5fba88a97b8ffca1e9b9aa5a92bec8fbd
Adin Solomon <adinbsolomon@gmail.com>
2022-01-26 20:11:26 -0500

    Filter Testing

---
+++ Filtration/FilterTest.py
@@ -0,0 +1,14 @@
+
+import os
+
+from RegionFilter import RegionFilter
+
+test_dir = None
+test_files = os.listdir(test_dir)
+
+labels = None
+
+def test_filter(filter: RegionFilter):
+
+    ### TODO
+    pass
