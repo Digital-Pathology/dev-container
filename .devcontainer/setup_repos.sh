2237e1180c9a515154af95c6ce338ed039505a3f
Ritik Ghanshani <ghanshaniritik@gmail.com>
2022-02-10 18:25:28 -0500

    initial commit

---
+++ .devcontainer/setup_repos.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+goto(){
+    cd "/workspaces/$1"
+}
+
+clone_repo(){
+
+    if [[ ! -d "/workspaces/dev-container/${1}/.git" && -d "/workspaces/dev-container/${1}" ]]; then
+        echo "Removing ${1} ..."
+        rm -rf "/workspaces/dev-container/${1}"
+    fi
+
+    goto "dev-container"
+    if [[ ! -d "/workspaces/dev-container/${1}" ]]; then
+        git clone "https://${USER}:${TOKEN}@github.com/Digital-Pathology/${1}.git" -b ${2}
+        goto "dev-container/${1}"
+        git remote set-url origin "git@github.com:Digital-Pathology/${1}.git"
+    fi
+}
+
+. ./.devcontainer/credentials
+
+clone_repo UnifiedImageReader main
+clone_repo WebApp main
+clone_repo ModelManager main
+clone_repo Filtration main
+clone_repo CustomDataset main
+clone_repo ML-Supervised main
+clone_repo ML-Unsupervised main
+clone_repo PublicDatasetMaterial main
